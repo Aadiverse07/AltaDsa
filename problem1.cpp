c3bc42af558c34dc61fc4862f530459e733309b2
Aadiverse07 <devrajsinghsingh883@gmail.com>
2026-09-12 22:24:15 +0530

    Add files via upload

---
+++ problem1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int year;
+ cout<<"Enter The number of days you have lived"<<endl;
+ cin>>year;
+ cout<<year/365<<"years ";
+ cout<<year%365<<" days"<<endl;
+ 
+
+ return 0;
+}
