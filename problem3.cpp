03f088882d55a1e7f85647d99f0ffc5f84a3813e
Aadiverse07 <devrajsinghsingh883@gmail.com>
2026-09-14 23:52:37 +0530

    Add files via upload

---
+++ problem3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, temporary;
+    cout <<"Enter the initial value of a:-";
+    cin >> a;
+    cout <<"Enter the initial value of b:-";
+    cin >>b; 
+
+  cout << "Before swap: a = " << a << endl;
+  cout << "Before swap: b = " << b << endl;
+
+    temporary = a;
+    a = b;
+    b = temporary;
+
+    // After swapping
+    cout << "After swap: a = " << a << endl;
+    cout << "After swap: b = " << b << endl;
+
+    return 0;
+}
