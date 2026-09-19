571e39228988c95516281d9cebdceb5ba00c2f23
Aadiverse07 <devrajsinghsingh883@gmail.com>
2026-09-19 22:21:48 +0530

    Add files via upload

---
+++ problem15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cout<<"Enter the value of N: ";
+    cin>>N;
+    while(N>=1)
+     {
+      cout << N;
+        N--;
+     }
+}
